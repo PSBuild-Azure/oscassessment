9e20cc0bddf1154781cec192fe4042901c5fa046
fabstao <fabian@nuo.com.mx>
2020-11-30 12:13:03 -0500

    Fixed

--- osp-sizing.sh
+++ osp-sizing.sh
@@ -23,48 +23,61 @@ spacedel
 outlabel "## Openstack projects/tenants"
 outlabel "\`\`\`"
 nice -n 19 openstack project list >> $output
-sleep 3
+sleep 2
 outlabel "\`\`\`"
 outlabel "\`\`\`"
 nice -n 19 openstack usage list >> $output
-sleep 3
+sleep 2
 outlabel "\`\`\`"
+
 outlabel "## Cloud Instances" 
 outlabel "\`\`\`"
 nice -n 19 openstack server list --all-projects >> $output
-sleep 3
+sleep 2
 outlabel "\`\`\`"
+
+tenants=$(nice -n 19 openstack project list -f value | awk '/services/ {next} {print $2}')
+outlabel "## Openstack Compute Flavors" 
+outlabel "\`\`\`"
+nice -n 19 openstack flavor list --all $i >> $output
+sleep 2
+outlabel "\`\`\`"
+
 outlabel "## Cinder volumes"
 outlabel "\`\`\`"
 nice -n 19 openstack volume list --all-projects >> $output
-sleep 3
+sleep 2
 outlabel "\`\`\`"
+
 outlabel "## Redes (Neutron)"
 outlabel "\`\`\`"
-tenants=$(nice -n 19 openstack project list -f value | awk '/services/ {next} {print $2}')
 for i in $tenants
 do
 	outlabel "### Redes en proyecto: $i"
 	nice -n 19 openstack network list --project $i >> $output
-	sleep 3
+	sleep 2
 done
 outlabel "\`\`\`"
+
 outlabel "\`\`\`"
 spacedel
 nice -n 19 openstack subnet list  >> $output
-sleep 3
+sleep 2
 outlabel "\`\`\`"
+
 outlabel "\`\`\`"
 nice -n 19 openstack subnet pool list  >> $output
-sleep 3
+sleep 2
 outlabel "\`\`\`"
+
 outlabel "## Hosts"
 outlabel "\`\`\`"
 nice -n 19 openstack host list >> $output
-sleep 3
+sleep 2
 nice -n 19 openstack hypervisor stats show >> $output
-sleep 3
+sleep 2
 outlabel "\`\`\`"
+
 outlabel "\`\`\`"
 hosts=$(nice -n 19 openstack hypervisor list -f value | awk '{print $2}')
 for i in $hosts
@@ -73,8 +86,10 @@ do
 	sleep 2
 done
 outlabel "\`\`\`"
+
 vms=$(nice -n 19 openstack server list --all-projects -f value | awk '{print $2}')
-sleep 3
+sleep 2
+
 outlabel "## Detalle de VMs"
 outlabel "\`\`\`"
 for i in $vms
